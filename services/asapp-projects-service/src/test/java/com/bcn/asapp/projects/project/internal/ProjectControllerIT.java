d500847d51a2fe1a8b95ec939d9b6797ededa299
attrigo <tonitrigodiaz@gmail.com>
2025-03-15 13:55:03 +0100

    chore: address compilation warning
    Explicitly ignore the mapping of the field tasks of toProjectDTO
    Change the deprecated annotation @MockBean by @MockitoBean in ITs

--- services/asapp-projects-service/src/test/java/com/bcn/asapp/projects/project/internal/ProjectControllerIT.java
+++ services/asapp-projects-service/src/test/java/com/bcn/asapp/projects/project/internal/ProjectControllerIT.java
@@ -46,8 +46,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -67,7 +67,7 @@ class ProjectControllerIT {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private ProjectService projectServiceMock;
 
     private UUID fakeProjectId;
