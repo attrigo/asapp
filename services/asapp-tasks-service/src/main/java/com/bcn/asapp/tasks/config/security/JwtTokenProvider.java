9d957976f4d33538ec32b2fffff66218cfdfd5dc
attrigo <tonitrigodiaz@gmail.com>
2025-04-19 21:02:40 +0200

    chore: improve Javadoc related with JWT
    Replace "JWT token" with "JWT"

--- services/asapp-tasks-service/src/main/java/com/bcn/asapp/tasks/config/security/JwtTokenProvider.java
+++ services/asapp-tasks-service/src/main/java/com/bcn/asapp/tasks/config/security/JwtTokenProvider.java
@@ -59,9 +59,9 @@ public class JwtTokenProvider {
     }
 
     /**
-     * Extracts the username (subject) from the provided JWT token.
+     * Extracts the username (subject) from the provided JWT.
      *
-     * @param token the JWT token, must not be {@literal null}.
+     * @param token the JWT, must not be {@literal null}.
      * @return an {@link Optional} containing the username (subject) if present, or an empty {@link Optional} if not.
      * @throws IllegalArgumentException if the token is null or empty.
      */
@@ -79,9 +79,9 @@ public class JwtTokenProvider {
     }
 
     /**
-     * Extracts the list of authorities (roles) from the provided JWT token.
+     * Extracts the list of authorities (roles) from the provided JWT.
      *
-     * @param token the JWT token, must not be {@literal null}.
+     * @param token the JWT, must not be {@literal null}.
      * @return a list containing the role associated with the token, or an empty list if no role claim is found.
      * @throws IllegalArgumentException if the token is null or empty.
      */
@@ -103,9 +103,9 @@ public class JwtTokenProvider {
     }
 
     /**
-     * Validates the provided JWT token by checking its signature.
+     * Validates the provided JWT by checking its signature.
      *
-     * @param token the JWT token to validate.
+     * @param token the JWT to validate.
      * @return true if the token is valid, otherwise false.
      */
     public boolean validateToken(String token) {
@@ -121,11 +121,11 @@ public class JwtTokenProvider {
         } catch (SignatureException e) {
             logger.error("Invalid JWT signature: {}", e.getMessage());
         } catch (MalformedJwtException e) {
-            logger.error("Invalid JWT token: {}", e.getMessage());
+            logger.error("Invalid JWT: {}", e.getMessage());
         } catch (ExpiredJwtException e) {
-            logger.error("JWT token is expired: {}", e.getMessage());
+            logger.error("JWT is expired: {}", e.getMessage());
         } catch (UnsupportedJwtException e) {
-            logger.error("JWT token is unsupported: {}", e.getMessage());
+            logger.error("JWT is unsupported: {}", e.getMessage());
         } catch (IllegalArgumentException e) {
             logger.error("JWT claims string is empty: {}", e.getMessage());
         }
