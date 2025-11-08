2558fbc0cf6a99d026cb4508aa31bb4e520e37cd
attrigo <tonitrigodiaz@gmail.com>
2025-11-08 14:36:10 +0100

    refactor(users): rename `newUser` to `create` and `reconstructedUser` to `reconstitute` for clarity and consistency
    - Updated user creation and reconstitution method names in the `User` domain model.
    - Adjusted related method calls in services, tests, and mappers.
    - Rephrased associated comments and documentation for improved clarity.

--- services/asapp-users-service/src/main/java/com/bcn/asapp/users/application/user/out/UserRepository.java
+++ services/asapp-users-service/src/main/java/com/bcn/asapp/users/application/user/out/UserRepository.java
@@ -52,7 +52,7 @@ public interface UserRepository {
      * <p>
      * If the user is new (without ID), it will be persisted and returned with a generated ID.
      * <p>
-     * If the user is reconstructed (with ID), it will be updated.
+     * If the user is reconstituted (with ID), it will be updated.
      *
      * @param user the {@link User} to save
      * @return the saved {@link User} with a persistent ID
