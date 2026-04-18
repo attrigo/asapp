409ca266b0416318fb17863ac4cbad7c26c75566
attrigo <tonitrigodiaz@gmail.com>
2026-04-18 17:06:23 +0200

    chore: release version 0.2.0

--- services/asapp-users-service/src/main/java/com/bcn/asapp/users/infrastructure/config/OpenApiConfiguration.java
+++ services/asapp-users-service/src/main/java/com/bcn/asapp/users/infrastructure/config/OpenApiConfiguration.java
@@ -36,6 +36,6 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
  * @author attrigo
  */
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Users Service API", version = "0.2.0-SNAPSHOT", description = "Provides users operations", license = @License(name = "Apache-2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")))
+@OpenAPIDefinition(info = @Info(title = "Users Service API", version = "0.2.0", description = "Provides users operations", license = @License(name = "Apache-2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")))
 @SecurityScheme(type = SecuritySchemeType.HTTP, name = "Bearer Authentication", scheme = "bearer", bearerFormat = "JWT")
 public class OpenApiConfiguration {}
