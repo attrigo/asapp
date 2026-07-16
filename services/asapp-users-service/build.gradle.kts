a546e0b4a9a69be87acbca36945b4cd9a504a244
attrigo <tonitrigodiaz@gmail.com>
2026-07-16 21:53:12 +0200

    build(gradle): wire users-service dependencies

--- services/asapp-users-service/build.gradle.kts
+++ services/asapp-users-service/build.gradle.kts
@@ -1,1 +1,24 @@
-// populated in a later Gradle migration subtask
+import org.gradle.api.artifacts.VersionCatalogsExtension
+
+plugins {
+    id("asapp.domain-service-conventions")
+}
+
+val libs = extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
+
+dependencies {
+    implementation(project(":libs:asapp-http-clients"))
+    implementation("org.springframework.boot:spring-boot-starter-aspectj")
+    implementation("org.springframework.boot:spring-boot-starter-restclient")
+    implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
+    implementation(libs.findLibrary("resilience4j-spring-boot4").get())
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-aspectj-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-restclient-test")
+    testImplementation(libs.findLibrary("mockserver-client-java").get())
+    testImplementation(libs.findLibrary("mockserver-netty").get())
+    testImplementation(libs.findLibrary("testcontainers-mockserver").get())
+}
