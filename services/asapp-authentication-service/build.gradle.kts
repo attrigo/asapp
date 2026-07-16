7a1f3f1e525a150977781e57db02c14cd50fd5d4
attrigo <tonitrigodiaz@gmail.com>
2026-07-16 21:47:06 +0200

    build(gradle): wire authentication-service dependencies

--- services/asapp-authentication-service/build.gradle.kts
+++ services/asapp-authentication-service/build.gradle.kts
@@ -1,1 +1,15 @@
-// populated in a later Gradle migration subtask
+import org.gradle.api.artifacts.VersionCatalogsExtension
+
+plugins {
+    id("asapp.domain-service-conventions")
+}
+
+val libs = extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
+
+dependencies {
+    // Required by Spring Security PasswordEncoderFactories
+    implementation(libs.findLibrary("bcprov-jdk18on").get())
+    // Must be compile-scope to be used by JdbcConversionsConfiguration
+    implementation("org.postgresql:postgresql")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+}
